9c606c05673b1ceaa4d3786a83aff6b1d6505448
arunanksharan <arunank.sharan@gmail.com>
2023-10-24 13:10:54 +0530

    4. Added /messages/list route with controller and placeholder data which is returned from service

--- projects/project_1/src/messages/messages.service.ts
+++ projects/project_1/src/messages/messages.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  listMessages() {
+    return [
+      {
+        id: 1,
+        text: 'First message',
+      },
+      {
+        id: 2,
+        text: 'Second message',
+      },
+      {
+        id: 3,
+        text: 'Third message',
+      },
+    ];
+  }
+}
