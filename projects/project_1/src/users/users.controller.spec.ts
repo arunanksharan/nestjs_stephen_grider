dabbaa6ccc8a4f6503b094564daf7e8f6776eacf
arunanksharan <arunank.sharan@gmail.com>
2023-11-08 12:06:36 +0530

    added testing suite for unit testing controller

--- projects/project_1/src/users/users.controller.spec.ts
+++ projects/project_1/src/users/users.controller.spec.ts
@@ -1,12 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { UserEntity } from './users.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let dummyUsersService: Partial<UsersService>;
+  let dummyAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    dummyUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'asdf@asdf.com',
+          password: 'asdf',
+        } as UserEntity);
+      },
+      find: (email: string) => {
+        return Promise.resolve([
+          {
+            id: 1,
+            email,
+            password: 'asdf',
+          } as UserEntity,
+        ]);
+      },
+      // remove: () => {},
+      // update: () => {},
+    };
+    dummyAuthService = {
+      // signup: () => {},
+      signin: (email, password) => {
+        return Promise.resolve({
+          id: 1,
+          email,
+          password,
+        } as UserEntity);
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: dummyUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: dummyAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +61,31 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('asdf@asdf.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('asdf@asdf.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    dummyUsersService.findOne = () => Promise.resolve(null);
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      { email: 'asdf@asdf.com', password: '12345678' },
+      session,
+    );
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
--- projects/project_1/src/users/users.controller.ts
+++ projects/project_1/src/users/users.controller.ts
@@ -20,7 +20,7 @@ import { UserDto } from './dtos/user.dto';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { UserEntity } from './users.entity';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
