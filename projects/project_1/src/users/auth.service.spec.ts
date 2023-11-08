44b22e9e438790af6ec14613db009c7068f291df
arunanksharan <arunank.sharan@gmail.com>
2023-11-08 10:33:21 +0530

    Refactors all tests to use service.signup and service.signin based on in-memory users array

--- projects/project_1/src/users/auth.service.spec.ts
+++ projects/project_1/src/users/auth.service.spec.ts
@@ -53,26 +53,34 @@ describe('AuthService', () => {
   });
 
   it('throws an error if user signs up with email that is in use', async () => {
-    dummyUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a', password: '1' } as UserEntity]);
+    // dummyUsersService.find = () =>
+    //   Promise.resolve([{ id: 1, email: 'a', password: '1' } as UserEntity]);
+    // await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+    //   BadRequestException,
+    // );
+    await service.signup('asdf@asdf.com', 'asdf');
     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
       BadRequestException,
     );
   });
+
   it('throws if signin is called with an unused email', async () => {
     await expect(
       service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
     ).rejects.toThrow(NotFoundException);
   });
+
   it('throws if an invalid password is provided', async () => {
-    dummyUsersService.find = () =>
-      Promise.resolve([
-        { email: 'asdf@asdf.com', password: 'laskdjf' } as UserEntity,
-      ]);
-    await expect(
-      service.signin('laskdjf@alskdfj.com', 'password'),
-    ).rejects.toThrow(BadRequestException);
+    // dummyUsersService.find = () =>
+    //   Promise.resolve([
+    //     { email: 'asdf@asdf.com', password: 'laskdjf' } as UserEntity,
+    //   ]);
+    await service.signup('laskdjf@alskdfj.com', 'password');
+    await expect(service.signin('laskdjf@alskdfj.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
   });
+
   it('returns a user if a valid password is provided', async () => {
     await service.signup('asdf@asdf.com', 'mypassword');
     const user = await service.signin('asdf@asdf.com', 'mypassword');
